1e00f8c3aff075673585e2cc3f29e509863f4341
ilya3d <ilya@twinscom.ru>
2015-01-28 15:30:15 +0300

    binary search + tpl python construction

---
+++ python.py
@@ -0,0 +1,9 @@
+#read int
+n = int( input() )
+#read array
+a = [int(x) for x in input().split()]
+
+#for 0..n-1
+for i in range( n ):
+#for itm in array
+for z in q:
--- search/BinarySearch.py
+++ search/BinarySearch.py
@@ -1,28 +1,10 @@
-# import sys
-# n = int( input() )
-# a = list(map(int, input().split()))
-# m = int( input() )
-# q = list(map(int, input().split()))
-#
-# for i in range( 1, n ):
-#     a[i] = a[i-1] + a[i]
-#
-# b = list()
-# for i in range(m):
-#     z = q[i]
-#     l1 = 0
-#     l2 = n-1
-#     while True:
-#         ind = (l2 + l1) // 2
-#         if z > a[ind]:
-#             l1 = ind + 1
-#         elif ind > 0 and z <= a[ind-1]:
-#             l2 = ind
-#         else:
-#             ind += 1
-#             b.append(str(ind))
-#             break
-
-def binary_search():
-    #todo
-    return True
+def binary_search( a, z ):
+    lo = 0
+    hi = n
+    while lo < hi:
+        mid = (hi + lo) // 2
+        if z > a[mid]:
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
